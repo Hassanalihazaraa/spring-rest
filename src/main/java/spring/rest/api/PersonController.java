1e779aeaba52019ca6a17a0c60584348e13e38f2
Hassan <hassanalihazaraa@gmail.com>
2020-10-05 14:08:07 +0200

    get all people form the api

--- src/main/java/spring/rest/api/PersonController.java
+++ src/main/java/spring/rest/api/PersonController.java
@@ -1,13 +1,13 @@
 package spring.rest.api;
 
 
+import org.apache.tomcat.jni.ProcErrorCallback;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.rest.model.Person;
 import spring.rest.service.PersonService;
+
+import java.util.List;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -22,5 +22,10 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
     }
 }
