8639fb6effebe1c2d621b3d13d4e403ebbfa1a43
Hassan <hassanalihazaraa@gmail.com>
2020-10-05 14:37:24 +0200

    rest complete

--- src/main/java/spring/rest/service/PersonService.java
+++ src/main/java/spring/rest/service/PersonService.java
@@ -7,6 +7,8 @@ import spring.rest.dao.PersonDao;
 import spring.rest.model.Person;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PersonService {
@@ -23,5 +25,17 @@ public class PersonService {
 
     public List<Person> getAllPeople() {
         return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson) {
+        return personDao.updatePersonById(id, newPerson);
     }
 }
